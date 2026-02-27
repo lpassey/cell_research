be7f56dd14417cce158fa554bd837e85ece1fdce
Lee Passey <lee@novomail.net>
2026-02-27 13:27:36 -0800

    1. Added code to allow collection of comparison frequencies 2. moved decision to compare right or left to a method, so it could be disabled when required by "monkey patching" 3. Remove thread lock semaphore; moved to parent class 4. Reformatting to improve comprehension and readability

--- modules/multithread/BubbleSortCell.py
+++ modules/multithread/BubbleSortCell.py
@@ -15,61 +15,71 @@ class BubbleSortCell(MultiThreadCell):
     def within_boundary(self, pos):
         if pos[0] > self.right_boundary[0] or pos[1] > self.right_boundary[1]:
             return False
-        
+
         if pos[0] < self.left_boundary[0] or pos[1] < self.left_boundary[1]:
             return False
 
         return True
 
     def should_move(self):
+        self.status_probe.count_comparison()
         if self.reverse_direction:
             bigger_than_left = False
             if self.current_position[0] > self.left_boundary[0]:
-                bigger_than_left = self.value > self.cells[int(self.current_position[0] - 1)].value and self.cells[int(self.current_position[0] - 1)].status ==  CellStatus.ACTIVE 
-            smaller_than_right = False 
+                bigger_than_left = self.value > self.cells[int(self.current_position[0] - 1)].value and self.cells[int(self.current_position[0] - 1)].status ==  CellStatus.ACTIVE
+            smaller_than_right = False
             if self.current_position[0] < self.right_boundary[0]:
                 smaller_than_right = self.value < self.cells[int(self.current_position[0] + 1)].value and  self.cells[int(self.current_position[0] + 1)].status ==  CellStatus.ACTIVE
-            
+
             return  bigger_than_left or  smaller_than_right
-        
+
         smaller_than_left = False
         if self.current_position[0] > self.left_boundary[0]:
-            smaller_than_left = self.value < self.cells[int(self.current_position[0] - 1)].value and self.cells[int(self.current_position[0] - 1)].status ==  CellStatus.ACTIVE 
-        bigger_than_right = False 
+            smaller_than_left = self.value < self.cells[int(self.current_position[0] - 1)].value and self.cells[int(self.current_position[0] - 1)].status ==  CellStatus.ACTIVE
+        bigger_than_right = False
         if self.current_position[0] < self.right_boundary[0]:
             bigger_than_right = self.value > self.cells[int(self.current_position[0] + 1)].value and  self.cells[int(self.current_position[0] + 1)].status ==  CellStatus.ACTIVE
-            
+
         return smaller_than_left or bigger_than_right
 
     def should_move_to(self, target_position, check_right):
-            
         if (
             self.status == CellStatus.ACTIVE
             and self.within_boundary(target_position)
-            and (self.cells[int(target_position[0])].status == CellStatus.ACTIVE  or self.cells[int(target_position[0])].status == CellStatus.FREEZE)
+            and (   self.cells[int(target_position[0])].status == CellStatus.ACTIVE
+                 or self.cells[int(target_position[0])].status == CellStatus.FREEZE)
         ):
             err_happen = random.random() < 0
             if err_happen:
-                return not self.value > self.cells[int(target_position[0])].value 
+                return not self.value > self.cells[int(target_position[0])].value
             if self.reverse_direction:
                 return self.value < self.cells[int(target_position[0])].value if check_right else self.value > self.cells[int(target_position[0])].value
-            return self.value > self.cells[int(target_position[0])].value if check_right else self.value < self.cells[int(target_position[0])].value
-    
-    def move(self):
-        self.lock.acquire()
-        self.with_lock = True
-        if self.group.status == GroupStatus.SLEEP and self.status != CellStatus.MOVING:
-            self.status = CellStatus.SLEEP
-        if self.should_move():
-            self.status_probe.record_compare_and_swap()
-        # new logic - random check left or right
-        check_right = random.random() < 0.5
-        if check_right:
-            target_position = (self.current_position[0] + self.cell_vision, self.current_position[1])
-        else:
-            target_position = (self.current_position[0] - self.cell_vision, self.current_position[1])
-        if self.should_move_to(target_position, check_right):
-            self.swap(target_position)
-        self.lock.release()
-        self.with_lock = False
+            if check_right:
+                return self.value > self.cells[int(target_position[0])].value
+            else:
+                return self.value < self.cells[int(target_position[0])].value
 
+    def flip_coin(self):
+        r = random.random()
+        return r < 0.5
+
+
+    def move(self):
+         # with self.lock:
+            self.with_lock = True
+            if self.group.status == GroupStatus.SLEEP and self.status != CellStatus.MOVING:
+                self.status = CellStatus.SLEEP
+            if self.should_move():
+                self.status_probe.record_compare_and_swap()
+            # new logic - random check left or right
+            check_right = self.flip_coin()
+            if check_right:
+                target_position = (self.current_position[0] + self.cell_vision, self.current_position[1])
+            else:
+                target_position = (self.current_position[0] - self.cell_vision, self.current_position[1])
+            swapped = 0;
+            if self.should_move_to(target_position, check_right):
+                self.swap(target_position)
+                swapped = 1
+            self.with_lock = False
+            return swapped
